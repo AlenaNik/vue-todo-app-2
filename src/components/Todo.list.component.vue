b99cc51a5cabe72db8bd0282d10296f97d927455
AlenaNik <alena.nik.digital@gmail.com>
2019-09-28 23:15:57 +0200

    remove items

--- src/components/Todo.list.component.vue
+++ src/components/Todo.list.component.vue
@@ -6,43 +6,59 @@
            v-model="newTodo"
            @keyup.enter="addTodo"
     >
-    <div v-for="todo in todos" :key="todo.id" class="todo-item">
+    <div v-for="(todo, index) in todos" :key="todo.id" class="todo-item">
       {{ todo.title }}
+      <div class="remove-items" @click="removeTodo(index)">
+        &times;
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'Todo',
-    data() {
-      return {
-        newTodo: '',
-        todos: [
-          {
-            'id': 1,
-            'title': 'Finish tutorial',
-            'completed': false,
-          },
-          {
-            'id': 2,
-            'title': 'Take over world',
-            'completed': false,
-          },
-          {
-            'id': 3,
-            'title': 'Feed a cat and take over world',
-            'completed': false,
-          },
-        ]
-      };
-    },
+  name: 'Todo',
+  data() {
+    return {
+      newTodo: '',
+      idForTodo: 4,
+      todos: [
+        {
+          'id': 1,
+          'title': 'Finish tutorial',
+          'completed': false,
+        },
+        {
+          'id': 2,
+          'title': 'Take over world',
+          'completed': false,
+        },
+        {
+          'id': 3,
+          'title': 'Feed a cat and take over world',
+          'completed': false,
+        },
+      ]
+    };
+  },
   methods: {
-      addTodo() {
-        alert('adding')
+    addTodo() {
+      if (this.newTodo.trim().length === 0) {
+        return
       }
+      this.todos.push({
+        id: this.idForTodo,
+        title: this.newTodo,
+        completed: false,
+      })
+      this.newTodo = ''
+      this.idForTodo++
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1)
+    }
   }
-  };
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -57,5 +73,20 @@ export default {
     outline: 0;
   }
 }
+
+  .todo-item {
+    margin-bottom: 12px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .remove-items {
+    cursor: pointer;
+    margin-left: 14px;
+    &:hover {
+      color: hotpink;
+    }
+  }
 
 </style>
